72caf15f882058feaede37e8ec01fae15f9fde9b
shortc1rcuit <shortc1rcuit0168@gmail.com>
2024-12-25 22:36:55 +0000

    Solved part 2 (day 1) in Haskell

--- AOC_Haskell/2024/day_01/day_1.hs
+++ AOC_Haskell/2024/day_01/day_1.hs
@@ -7,12 +7,10 @@ run_sol filename sol = do
     contents <- readFile filename
     print $ sol contents
 
-{-
-part_1 input = sum (zipWith abs_diff (sort left) (sort right))
+{- part_1 input = sum (zipWith abs_diff (sort left) (sort right))
     where
         (left, right) = parse input
-        abs_diff x y = abs (x - y)
--}
+        abs_diff x y = abs (x - y) -}
 
 --part_1 input = sum (zipWith ((abs .) . (-)) (sort left) (sort right))
 --part_1 input = ((sum .) . (. sort) . zipWith ((abs .) . (-)) . sort) left right
@@ -20,15 +18,27 @@ part_1 input = sum (zipWith abs_diff (sort left) (sort right))
 part_1 :: String -> Int
 part_1 = (uncurry ((sum .) . (. sort) . zipWith ((abs .) . (-)) . sort)) . parse
 
+{- part_2 :: String -> Int
+part_2 input = sum (map (\x -> x * (count_eq right x)) left)
+    where
+        count_eq l x = length (filter (\y -> x == y) l)
+        (left, right) = parse input -}
+
+part_2 :: String -> Int
+--part_2 input = sum (map ((*) <*> (count_eq right)) left)
+part_2 = uncurry ((sum .) . flip (map . ((*) <*>) . count_eq)) . parse
+    where
+        count_eq = (length .) . flip (filter . (==))
+
 {- parse :: String -> ([Int], [Int])
 parse input = (left, right)
     where
         [left, right] = lists
         lists = transpose (map (\x -> map read (words x)) (lines input)) -}
 
---parse :: String -> ([Int], [Int])
+parse :: String -> ([Int], [Int])
 --parse input = (,) ((lists input) !! 0) ((lists input) !! 1)
 --parse input = ((,) . (!! 0) . lists $ input) ((!! 1) . lists $ input)
-parse = ap ((,) . (!! 0) . lists) ((!! 1) . lists)
+parse = ((,) . (!! 0) . lists) <*> ((!! 1) . lists)
     where
         lists = transpose . map (map read . words) . lines
